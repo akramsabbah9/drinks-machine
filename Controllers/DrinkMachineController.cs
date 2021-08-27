871414b70565c32ba2d81072e8ae5410de30722f
Akram Sabbah <akramsabbah9@gmail.com>
2021-08-27 16:19:38 -0700

    added coinage to machine and preliminary post route

--- Controllers/DrinkMachineController.cs
+++ Controllers/DrinkMachineController.cs
@@ -11,22 +11,46 @@ namespace drinks_machine.Controllers
     [Route("[controller]")]
     public class DrinkMachineController : ControllerBase
     {
-        // make dictionary of Drink objects
+        // make dictionary of Drink objects, and list of Money
+
+        // dictionary used to lookup drinks when receiving purchase request
         private Dictionary<string, Drink> Drinks = new Dictionary<string, Drink>();
+
+        // list of change to easily scale up when adding new types of coinage
+        private List<Money> Change = new List<Money>();
 
         private readonly ILogger<DrinkMachineController> _logger;
 
         public DrinkMachineController(ILogger<DrinkMachineController> logger)
         {
             _logger = logger;
-            // initialize Drinks
+            // add drinks
             Drinks.Add("Coke", new Drink("Coke", 25, 5));
             Drinks.Add("Pepsi", new Drink("Pepsi", 36, 15));
             Drinks.Add("Soda", new Drink("Soda", 45, 3));
+
+            // add available money in machine
+            Change.Add(new Money(1, 100));
+            Change.Add(new Money(5, 10));
+            Change.Add(new Money(10, 5));
+            Change.Add(new Money(25, 25));
+
+            // in case later devs add change out of order, sort least to greatest
+            Change.Sort((a, b) => a.Value.CompareTo(b.Value));
         }
 
+        // get state of machine
         [HttpGet]
-        public Dictionary<string, Drink> Get()
+        public Dictionary<string, Drink> GetMachineContents()
+        {
+            Change.ForEach(Console.WriteLine); // debug: check change amounts
+            return Drinks;
+        }
+
+        // attempt to complete a transaction. If valid, return new amt of drinks
+        // and subtract Money from Change. Otherwise, 400 and refund.
+        [HttpPost]
+        public Dictionary<string, Drink> PurchaseDrinks()
         {
             return Drinks;
         }

975d9d9031814769092f9f2819cfb0560b2a1216
Akram Sabbah <akramsabbah9@gmail.com>
2021-08-27 21:53:57 -0700

    changed initialQuantity to quantity to fix binding issue on deserialization

--- Models/Money.cs
+++ Models/Money.cs
@@ -5,11 +5,11 @@ namespace drinks_machine
         public int Value { get; } // the value of this coinage
         public int Quantity { get; set; }
 
-        public Money(int value, int initialQuantity) {
+        public Money(int value, int quantity) {
             // if someone thinks it would be funny to initialize price/quantity
             // as negative, set them to zero instead
             Value = value >= 0 ? value : 0;
-            Quantity = initialQuantity >= 0 ? initialQuantity : 0;
+            Quantity = quantity >= 0 ? quantity : 0;
         }
 
         // override ToString for debug purposes

171d9b74483107d259028d4b11281a65ecc99769
Akram Sabbah <akramsabbah9@gmail.com>
2021-08-29 12:50:51 -0700

    for user payment and change returned, display total value and list specific coins

--- ClientApp/src/components/ReceiptModal.js
+++ ClientApp/src/components/ReceiptModal.js
@@ -1,9 +1,23 @@
 import React, { useEffect } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { coinName } from "../utils/helpers";
 
 // modal to display receipt of purchase after completed transaction.
 function ReceiptModal({ show, setModal, data }) {
-    const { drinks, total, change } = data;
+    const { drinks, total, payment, change } = data;
+
+    // helper function to tally total value of money
+    const totalMoney = money => (money)
+        ? money.map(c => c.value * c.quantity).reduce((p, n) => p + n)
+        : 0;
+
+    // helper function to list all coins in an array
+    const listMoney = money => (money)
+        ? money
+            .filter(c => c.quantity > 0)
+            .map(c => `${c.quantity} ${coinName(c.value, c.quantity)}`)
+            .join(", ")
+        : null;
 
     // when modal is closed, update show state
     const toggle = () => setModal(!show);
@@ -15,16 +29,17 @@ function ReceiptModal({ show, setModal, data }) {
             <h4>Purchase Details</h4>
             {/* display purchased drinks */}
             <div className="d-flex justify-content-end">
+                {/* list in format of NAME, X drinks */}
                 <ul>
                 {(data.drinks)
                     ? Object.keys(drinks)
                         .filter(d => drinks[d].quantity > 0)
                         .map(d => {
                             const cur = drinks[d];
-                            let quantity = cur.quantity === 1 ? "drink" : "drinks";
+                            let amt = cur.quantity === 1 ? "drink" : "drinks";
 
                             return <li key={d}>
-                                {cur.quantity} {cur.name} {quantity}
+                                {cur.name}, {cur.quantity} {amt}
                             </li>;
                         })
                     : null
@@ -32,14 +47,34 @@ function ReceiptModal({ show, setModal, data }) {
                 </ul>
             </div>
             <br />
+
             <h4>Total Cost</h4>
             <div className="d-flex justify-content-end">
-                <p className="float-right">{total} cents</p>
+                <p>{total} cents</p>
             </div>
             <br />
+
+            <h4>Your Payment</h4>
+            {/* total payment */}
+            <div className="d-flex justify-content-end">
+                <p>{totalMoney(payment)} cents</p>
+            </div>
+            {/* coinage of payment */}
+            <div className="d-flex justify-content-end">
+                <p>({listMoney(payment)})</p>
+            </div>
+            <br />
+
             <h4>Change Received</h4>
-            <p className="float-right">{
-                (change) ? change.map(c => c.value * c.quantity).reduce((p, n) => p + n) : 0} cents</p>
+            {/* total change */}
+            <div className="d-flex justify-content-end">
+                <p>{totalMoney(change)} cents
+                </p>
+            </div>
+            {/* coinage of change */}
+            <div className="d-flex justify-content-end">
+                <p>({listMoney(change)})</p>
+            </div>
 
         </ModalBody>
         <ModalFooter>

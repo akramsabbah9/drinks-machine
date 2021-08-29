a7c2ef152cb5cec220ae051012007c943838654f
Akram Sabbah <akramsabbah9@gmail.com>
2021-08-28 19:15:42 -0700

    added coininput component to add coins for payment

---
+++ ClientApp/src/components/CoinInput.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// a single reusable input for a coin. updates its state on change.
+function CoinInput({ index, coin, state }) {
+    const [coins, setCoins] = state; // grab state from props
+
+    // update coins state when this input is changed
+    const handleChange = event => {
+        const { type, name, value } = event.target;
+
+        const quantity = parseInt(value) || 0; // short circuit to zero
+        
+        // deep copy coins and set new value
+        const newCoins = [ ...coins ];
+        newCoins[index] = { ...coin, quantity };
+        console.log(newCoins);
+        
+        setCoins(newCoins);
+    };
+
+    return (<div className="d-flex flex-column">
+        <label htmlFor={coin.name} className="h5">{coin.name}</label>
+        <input
+            type="number"
+            name={coin.name}
+            id={coin.name}
+            min="0"
+            onChange={handleChange}
+            className="w-50"
+        />
+    </div>);
+}
+
+export default CoinInput;
